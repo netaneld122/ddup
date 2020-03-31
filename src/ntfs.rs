84b685b33fe708b254a453fa66c2ced448399bf0
netaneld122 <netaneld122@gmail.com>
2020-03-31 16:19:16 +0300

    Implemented a simple usn data iteration (only for filenames)

--- src/ntfs.rs
+++ src/ntfs.rs
@@ -14,8 +14,8 @@ use winapi::um::winioctl::{
 };
 use winapi::um::winnt::LARGE_INTEGER;
 
-#[repr(packed)]
-#[derive(Default)]
+#[repr(C)]
+#[derive(Default, Debug)]
 pub struct USN_JOURNAL_DATA {
     pub UsnJournalID: DWORDLONG,
     pub FirstUsn: USN,
@@ -26,14 +26,14 @@ pub struct USN_JOURNAL_DATA {
     pub AllocationDelta: DWORDLONG,
 }
 
-#[repr(packed)]
+#[repr(C)]
 #[derive(Default)]
 pub struct CREATE_USN_JOURNAL_DATA {
     pub MaximumSize: DWORDLONG,
     pub AllocationDelta: DWORDLONG,
 }
 
-#[repr(packed)]
+#[repr(C)]
 #[derive(Default)]
 pub struct MFT_ENUM_DATA {
     pub StartFileReferenceNumber: DWORDLONG,
@@ -41,7 +41,7 @@ pub struct MFT_ENUM_DATA {
     pub HighUsn: USN,
 }
 
-#[repr(packed)]
+#[repr(C)]
 pub struct USN_RECORD {
     pub RecordLength: DWORD,
     pub MajorVersion: WORD,
@@ -56,13 +56,52 @@ pub struct USN_RECORD {
     pub FileAttributes: DWORD,
     pub FileNameLength: WORD,
     pub FileNameOffset: WORD,
-    pub FileName: [WCHAR; 16],
+    pub FileName: [WCHAR; 1],
+}
+
+pub struct UsnRecord {
+    pub filename: String,
+}
+
+pub struct UsnData {
+    buffer: [u8; 0x10000],
+    size: usize,
+    offset: usize,
+}
+
+impl Iterator for UsnData {
+    type Item = UsnRecord;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset >= self.size {
+            return None;
+        }
+
+        let base = self.buffer.as_ptr();
+        let ptr = unsafe { base.offset(self.offset as isize) };
+        let usn_record: &USN_RECORD = unsafe { std::mem::transmute(ptr) };
+
+        let filename = unsafe {
+            ptr.offset(usn_record.FileNameOffset as isize) as *const u16
+        };
+        let filename = unsafe {
+            std::slice::from_raw_parts(
+                filename,
+                (usn_record.FileNameLength / 2) as usize)
+        };
+        let filename = String::from_utf16_lossy(filename);
+
+        // Advance to next record
+        self.offset += usn_record.RecordLength as usize;
+
+        Some(UsnRecord { filename })
+    }
 }
 
 pub trait Ntfs {
     fn create_usn_journal(&self) -> Result<(), Error>;
     fn query_usn_journal(&self) -> Result<USN_JOURNAL_DATA, Error>;
-    fn enum_usn_data(&self, low: USN, high: USN) -> Result<USN_RECORD, Error>;
+    fn enum_usn_data(&self, low: USN, high: USN) -> Result<UsnData, Error>;
 }
 
 impl Ntfs for Volume {
@@ -115,14 +154,14 @@ impl Ntfs for Volume {
         }
     }
 
-    fn enum_usn_data(&self, low: USN, high: USN) -> Result<USN_RECORD, Error> {
+    fn enum_usn_data(&self, low: USN, high: USN) -> Result<UsnData, Error> {
         let mut returned_bytes: u32 = 0;
         let mft_enum_data = MFT_ENUM_DATA {
             StartFileReferenceNumber: 0,
             LowUsn: low,
             HighUsn: high,
         };
-        let mut records = [0u8; std::mem::size_of::<USN>() + 0x10000];
+        let mut buffer = [0u8; 0x10000];
 
         let res = unsafe {
             DeviceIoControl(
@@ -130,20 +169,24 @@ impl Ntfs for Volume {
                 FSCTL_ENUM_USN_DATA,
                 &mft_enum_data as *const MFT_ENUM_DATA as LPVOID,
                 std::mem::size_of_val(&mft_enum_data) as DWORD,
-                records.as_mut_ptr() as *mut USN_RECORD as LPVOID,
-                records.len() as DWORD,
+                buffer.as_mut_ptr() as *mut USN_RECORD as LPVOID,
+                buffer.len() as DWORD,
                 &mut returned_bytes as LPDWORD,
                 null_mut(),
             )
         };
 
-        // Skip USN
-        let records = &records[std::mem::size_of::<USN>()..];
-        let usn_record: USN_RECORD = unsafe { std::mem::transmute_copy(&records.as_ptr()) };
+        if res == 0 {
+            return Err(Error::last_os_error());
+        }
 
         match res {
             0 => Err(Error::last_os_error()),
-            _ => Ok(usn_record),
+            _ => Ok(UsnData {
+                buffer,
+                offset: std::mem::size_of::<USN>(), // Skip USN header
+                size: returned_bytes as usize,
+            }),
         }
     }
 }
--- tests/sanity.rs
+++ tests/sanity.rs
@@ -2,14 +2,16 @@ use ddup::Ntfs;
 use ddup::Volume;
 
 #[test]
-fn enumerate_one_mft_record() -> Result<(), std::io::Error> {
+fn enumerate_mft_records() -> Result<(), std::io::Error> {
     let volume = Volume::open(r"\\.\C:")?;
     volume.create_usn_journal()?;
     let journal = volume.query_usn_journal()?;
-    let record = volume.enum_usn_data(journal.LowestValidUsn, journal.NextUsn)?;
+    println!("{:#?}", journal);
 
-    let file_name = String::from_utf16_lossy(&{ record.FileName });
-    println!("{:?}", file_name);
+    let data = volume.enum_usn_data(journal.LowestValidUsn, journal.NextUsn)?;
+    for record in data {
+        println!("{}", record.filename);
+    }
 
     Ok(())
 }
